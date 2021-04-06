7306041d36a81624c50a8d599d32858d30ae8a05
payne <loonycoder@163.com>
2021-04-06 19:04:48 +0800

    post detail optimize
    fix

--- dao/mysql/post.go
+++ dao/mysql/post.go
@@ -10,3 +10,12 @@ func CreatePost(post *models.Post) (err error) {
 	_, err = db.Exec(sqlStr, post.ID, post.Title, post.Content, post.AuthorID, post.CommunityID)
 	return
 }
+
+func GetPostById(pid int64) (post *models.Post, err error) {
+	post = new(models.Post)
+	sqlStr := `select post_id , title,content , author_id , community_id , create_time
+				from post
+				where post_id = ?`
+	err = db.Get(post, sqlStr, pid)
+	return
+}
